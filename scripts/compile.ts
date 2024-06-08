6d755be41413bb908c2654c51fe8fea959206de0
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2024-06-08 12:44:37 +0200

    :heavy_plus_sign: Adding basic compilation script

---
+++ scripts/compile.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+import process from "process";
+import { Cell } from "ton-core";
+import { compileFunc } from "@ton-community/func-js";
+
+const HEX_ARTIFACTS_FOLDER = "./build";
+const SOURCE_FOLDER = "./contracts";
+const sourceFileName = process.argv[2];
+
+async function compileScript() {
+    console.log("Compiling contract: ", `${sourceFileName}.fc`);
+
+  // Compile source file
+  const compileResult = await compileFunc({
+    targets: [`${SOURCE_FOLDER}/${sourceFileName}.fc`],
+    sources: (x) => fs.readFileSync(x).toString("utf-8"),
+  });
+  if (compileResult.status === "error") {
+    console.error("Compilation Error: ", compileResult.message);
+    process.exit(1);
+  }
+
+  // Save compiled code in hex format
+  const cellBoc = Cell.fromBoc(Buffer.from(compileResult.codeBoc, "base64"))[0]
+    .toBoc()
+    .toString("hex");
+  if (!fs.existsSync(HEX_ARTIFACTS_FOLDER)) {
+    fs.mkdirSync(HEX_ARTIFACTS_FOLDER);
+  }
+  fs.writeFileSync(
+    `${HEX_ARTIFACTS_FOLDER}/${sourceFileName}.compiled.json`,
+    JSON.stringify({ hex: cellBoc })
+  );
+
+    console.log("Compilation successful");
+    console.log("Compiled code saved in: ", `${HEX_ARTIFACTS_FOLDER}/${sourceFileName}.compiled.json`);
+}
+compileScript();
